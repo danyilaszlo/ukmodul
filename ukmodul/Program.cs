76d9941469c084a887f0c85332300a78a60e2f73
danyilaszlo <42997044+danyilaszlo@users.noreply.github.com>
2018-10-10 07:34:21 +0200

    .

--- ukmodul/Program.cs
+++ ukmodul/Program.cs
@@ -17,9 +17,9 @@ namespace ukmodul
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            //Application.Run(new frmLogin());
+            Application.Run(new frmLogin());
 
-            Application.Run(new main());
+            //Application.Run(new main());
         }
     }
 }
