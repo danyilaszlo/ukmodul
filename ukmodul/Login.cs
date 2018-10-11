a690839e268c9125370481fa8f3b38bfbf19b88b
danyilaszlo <42997044+danyilaszlo@users.noreply.github.com>
2018-10-11 07:36:04 +0200

    .

--- ukmodul/Login.cs
+++ ukmodul/Login.cs
@@ -9,19 +9,15 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace ukmodul
 {
 
-
-
-
- 
 
     public partial class frmLogin : Form
     {
 
        // SqlConnection kapcs = new SqlConnection(@"Data Source=PC63\SQLEXPRESS;Initial Catalog=UKMODUL;Integrated Security=True");
-
 
 
           SqlConnection kapcs = new SqlConnection(@"Data Source=HPELITEBOOK\SQLEXPRESS;Initial Catalog=UKMODUL;Persist Security Info=True;User ID=sa;Password=SqlAdmin1");
@@ -34,7 +30,7 @@ namespace ukmodul
         private void btnLogin_Click(object sender, EventArgs e)
         {
  
-                         SqlDataAdapter sda = new SqlDataAdapter("SELECT * FROM felhasznalok WHERE felhaszn_nev = '"+ txtUsername.Text +"' AND passwd = '"+ txtPasswd.Text+"'", kapcs);
+            SqlDataAdapter sda = new SqlDataAdapter("SELECT * FROM felhasznalok WHERE felhaszn_nev = '"+ txtUsername.Text +"' AND passwd = '"+ txtPasswd.Text+"'", kapcs);
             DataTable dt = new DataTable();
             sda.Fill(dt);
             if (dt.Rows.Count == 1)
--- ukmodul/rendeles.cs
+++ ukmodul/rendeles.cs
@@ -9,16 +9,24 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data;
 using System.Data.SqlClient;
+using MyNamespace;
 
 namespace ukmodul
 {
     public partial class rendelesForm : Form
     {
 
-        
+        // SqlConnection kapcs = new SqlConnection(@"Data Source=PC63\SQLEXPRESS;Initial Catalog=UKMODUL;Integrated Security=True");
+
+
+        SqlConnection kapcs = new SqlConnection(@"Data Source=HPELITEBOOK\SQLEXPRESS;Initial Catalog=UKMODUL;Persist Security Info=True;User ID=sa;Password=SqlAdmin1");
+
+        DbKapcs();
+
         public rendelesForm()
         {
             InitializeComponent();
+
 
             
 
